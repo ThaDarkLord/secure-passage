dff26921407c46d227f6e76bca71f4f2db1c777c
ThaDarkLord <daniel.j.sykes92@gmail.com>
2023-12-22 20:10:04 -0500

    restructured js and added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,22 @@
 // Assignment Code
-// var generateBtn = document.querySelector("#generate");
 console.log(`Hello World`)
-var charUpperCase = [`A`, `B`, `C`, `D`,`E`,`F`, `G`, `H`, `I`,`J`,`K`, `L`, `M`, `N`,`O`,`P`, `Q`, `R`, `S`,`T`,`U`, `V`, `W`, `X`,`Y`,`Z`];
-var charLowerCase = [`a`, `b`,`c`,`d`,`e`,`f`,`g`,`h`,`i`,`j`,`k`,`l`,`m`,`n`,`o`,`p`,`q`,`r`,`s`,`t`,`u`,`v`,`w`,`x`,`y`,`z`];
-var num = [`0`,`1`,`2`,`3`,`4`,`5`,`6`,`7`,`8`,`9`];
-var specialChar =[`!`, `@`, `#`, `$`, `%`, `^`,`-`,`+`, `=`,`~`];
-var newPassword = [];
-var userChoices = [];
-var passLength = [];
+var charUpperCase = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`.split(``);
+var charLowerCase = `abcdefghijklmnopqrstuvwxyz`.split(``);
+var num = `01234567891`.split(``);
+var specialChar =`~!@#$%^&*()~+=`.split(``);
+
+// referneces the id used from the html
+var generateBtn = document.querySelector("#generate");
 
 
-// Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
+  function generatePassword(){
+    var newPassword = [];
+    var userChoices = [];
+
 var message = prompt(`How many characters would you like in your password?`);
 if(message > 128 || message < 8){
     alert(`Must stay within 8-128 character parameters`);
   } else{
-   
-    console.log(message)
-   
-    
   var lower = confirm(`Would you like your password to contain lowercase letters?`);
 if(lower){
   userChoices = userChoices.concat(charLowerCase);
@@ -58,23 +53,27 @@ if(!num){
   confirm(`are you sure?`)
 }
   }
-
 console.log(userChoices)
-console.log(message);
 
-for (let i = 0; i < message; i++) {
+// for loop goes through users chosen length and picks random values within the characters they selected.
+for (var i = 0; i < message; i++) {
   var randomIndex = Math.floor(Math.random() * userChoices.length)
-  console.log(randomIndex)
-  userChoices = randomIndex.toString()
-  newPassword = newPassword.push(userChoices)
- 
+  var additions = userChoices[randomIndex];
+  console.log(userChoices[randomIndex])
+  newPassword.push(additions)
+}
+
+return newPassword.join(``)
+  }
+
+// writePassword();
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
 }
 
 
-// }
-// writePassword();
-  
-
-
 // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword); 
+generateBtn.addEventListener("click", writePassword); 
